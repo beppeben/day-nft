d65de3124caa53db7efa485ae87d49f394f5a4cc
giuseppe <beppeben2030@gmail.com>
2022-10-02 17:48:45 +0200

    back to original sketch

--- day-nft-processor/node_p5.js
+++ day-nft-processor/node_p5.js
@@ -5,7 +5,7 @@ var sketch_path = args[0]
 var date = args[1]
 var message = args[2]
 var out_path = args[3]
-eval(fs.readFileSync(sketch_path + 'sketch_drinks.js')+'');
+eval(fs.readFileSync(sketch_path + 'sketch.js')+'');
 let resourcesToPreload = {
   flow_logo: p5.loadImage(sketch_path + 'flow_pixel.png'),
   stick_img: p5.loadImage(sketch_path + 'piggo_cut.png')
